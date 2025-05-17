c8012d4d33f551a6cf059bbc8db04d2fdb0787aa
evelyn <h2210024@nushigh.edu.sg>
2025-05-17 23:01:19 +0800

    yay

--- app.py
+++ app.py
@@ -1,15 +1,32 @@
-from flask import Flask, render_template, request, jsonify
 from utils import *
+
+from flask import Flask, redirect, url_for, session, render_template, request, jsonify
+from authlib.integrations.flask_client import OAuth
+import os
+from config import *
 
 
 app = Flask(__name__)
+app.secret_key = CLIENT_SECRET
+
+# OAuth Setup
+oauth = OAuth(app)
+google = oauth.register(
+    name='google',
+    client_id=CLIENT_ID,
+    client_secret=CLIENT_SECRET,
+    access_token_url='https://oauth2.googleapis.com/token',
+    access_token_params=None,
+    authorize_url='https://accounts.google.com/o/oauth2/auth',
+    authorize_params={'access_type': 'offline', 'prompt': 'consent'},
+    api_base_url='https://www.googleapis.com/oauth2/v1/',
+    client_kwargs={'scope': 'openid email profile'},
+    server_metadata_url='https://accounts.google.com/.well-known/openid-configuration',
+
+)
 
 @app.route("/")
 def home(): #bruhhhhhhhhhhhhhhhhh i was so confused pls oml
-    return render_template("index.html")
-
-@app.route("/main")
-def main():
     return render_template("landing_page.html")
 
 @app.route("/signup")
@@ -23,6 +40,24 @@ def signin():
 @app.route("/home_page")
 def home_page():
     return render_template("home_page.html")
+
+@app.route('/signup_w_google')
+def signup_w_google():
+    redirect_uri = url_for('auth_callback', _external=True)
+    return google.authorize_redirect(redirect_uri)
+
+@app.route('/auth/callback')
+def auth_callback():
+    token = google.authorize_access_token()
+    resp = google.get('userinfo')
+    user_info = resp.json()
+    session['email'] = user_info['email']
+    return redirect('/')
+
+@app.route('/logout')
+def logout():
+    session.pop('username', None)
+    return redirect(url_for('home'))
 
 if __name__ == "__main__":
     app.run(use_reloader=True, debug=True) # for auto-reloading cos yay
